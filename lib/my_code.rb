645bff38fd492bb132944b22a3b5d55622871b0a
Christina McMahon <christina.mcmahon1@gmail.com>
2019-11-27 00:35:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -56,5 +56,13 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
-  
+  flag = false
+  i = 0 
+  while i < source_array.length do
+    if source_array[i]
+      flag = true
+    end
+    i += 1
+  end
+  flag
 end
