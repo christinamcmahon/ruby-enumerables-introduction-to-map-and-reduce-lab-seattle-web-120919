9ad7e75393509b8de1e369f61ddbb26cd885dee6
Christina McMahon <christina.mcmahon1@gmail.com>
2019-11-27 00:14:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,12 +9,21 @@ def map_to_negativize(source_array)
 end
 
 def map_to_no_change(source_array)
+  result = []
+    
+  result
 end
 
 def map_to_double(source_array)
+  result = []
+  
+  result
 end
 
 def map_to_square(source_array)
+  result = []
+  
+  result
 end
 
 def reduce_to_total(source_array, starting_point)
