64628978c063f572aa79d8520f0ad654148fa6d1
Christina McMahon <christina.mcmahon1@gmail.com>
2019-11-27 00:20:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,10 +34,19 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
+  total = 0 
+  i = 0 
+  while i < source_array.length do 
+    total += source_array[i]
+    i += 1
+  end
+  total
 end
 
 def reduce_to_all_true(source_array)
+  
 end
 
 def reduce_to_any_true(source_array)
+  
 end
