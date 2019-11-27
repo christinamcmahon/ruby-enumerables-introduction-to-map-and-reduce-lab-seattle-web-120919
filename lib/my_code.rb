face5a7c9be42ce5d5971b05a1a5841c646ae40b
Christina McMahon <christina.mcmahon1@gmail.com>
2019-11-27 00:18:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,17 +15,17 @@ end
 
 def map_to_double(source_array)
   result = []
-  
-  result
-end
-
-def map_to_square(source_array)
-  result = []
   i = 0
   while i < source_array.length do
     result.push(source_array[i] * 2)
     i += 1
   end
+  result
+end
+
+def map_to_square(source_array)
+  result = []
+  
   result
 end
 
